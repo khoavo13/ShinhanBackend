a46e95f68b6ea8cb54da9dd99143219a4aa1b564
khoavo13 <vandangkhoa.vo@email.com>
2023-01-04 21:10:27 +0700

    Change banner spring boot

---
+++ SpringBoot_ChangeBanner/src/main/java/com/example/SpringBoot_ChangeBanner/SpringBootChangeBannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_ChangeBanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChangeBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChangeBannerApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_ChangeBanner/src/test/java/com/example/SpringBoot_ChangeBanner/SpringBootChangeBannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_ChangeBanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChangeBannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
